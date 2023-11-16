94614dedcaed9359105a35d8cd4066aa4787e890
Stoycho Donev <stoychod79@gmail.com>
2023-11-16 08:17:07 +0000

    feat: add python-dotenv package and a requirements.txt to the project
    extract sensitive settings in an .env file

--- backend/django_sessions/settings.py
+++ backend/django_sessions/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-18j+0r*@a)=dib5#zki%5b8$g@ktd@zd$p_70!*@v9&k4(2xj3'
+# Get the secret key from environment variable if exists,
+# set it to a dummy value otherwise
+SECRET_KEY = getenv('SECRET_KEY', 'very_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Get the DEBUG environment variable, default to False
+DEBUG = getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -83,12 +89,12 @@ WSGI_APPLICATION = 'django_sessions.wsgi.application'
 
 DATABASES = {
     'default': {
-        # Postgres database settings
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_backend',
-        'USER': 'django_backend',
-        'PASSWORD': 'django_backend',
-        'HOST': 'localhost',
+        # Get database settings from environment
+        'ENGINE': getenv('ENGINE'),
+        'NAME': getenv('NAME'),
+        'USER': getenv('USER'),
+        'PASSWORD': getenv('PASSWORD'),
+        'HOST': getenv('HOST'),
     }
 }
 
