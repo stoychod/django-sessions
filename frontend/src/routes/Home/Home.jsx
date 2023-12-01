75692a9925fd1327150715fcc85a27ede7585f25
Stoycho Donev <stoychod79@gmail.com>
2023-12-01 14:24:05 +0000

    feat(frontend): set up the Home page

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Container className="mt-5 p-5 bg-light">
+      <h1>Welcome to Session Auth</h1>
+      <p className="lead">
+        This is a React and Django application with session authentication and
+        CSRF token
+      </p>
+      <hr className="my-4" />
+      <p>Click the button below to login</p>
+      <Button variant="primary" as={Link} to={"/auth/login"}>
+        Login
+      </Button>
+    </Container>
+  );
 }
